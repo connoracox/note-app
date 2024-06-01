8eba328c762d1179725637e852f997031749c5a7
Connor Cox <connor.a.cox@gmail.com>
2024-06-01 07:39:24 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+}
+const App = () => {
+  const [notes, setNotes] = useState<
+    Note[]
+  >([
+    {
+      id: 1, 
+      title: "note title 1",
+      content: "note content 1"
+    },
+    {
+      id: 2, 
+      title: "note title 2",
+      content: "note content 2"
+    },    
+    {
+      id: 3, 
+      title: "note title 3",
+      content: "note content 3"
+    },    
+    {
+      id: 4, 
+      title: "note title 4",
+      content: "note content 4"
+    },
+  ]);
+
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+  return(
+  <div className="app-container">
+    <form className="note-form">
+      <input
+      value={title}
+      onChange={(event)=>
+      setTitle(event?.target.value)
+    }
+      placeholder="Title"
+      required
+      ></input>
+      <textarea 
+      placeholder="Notes"
+      rows={10}
+      required
+      ></textarea>
+      <button type="submit">
+        Add Note
+      </button>
+    </form>
+    <div className="notes-grid">
+      {notes.map((note)=> (
+      <div className="note-item">
+        <div className="notes-header">
+          <button>x</button>
+        </div>
+        <h2>{note.title}</h2>
+        <p>{note.content}</p>
+      </div>
+      ))}
     </div>
-  );
+  </div>
+  )
 }
 
-export default App;
+export default App
